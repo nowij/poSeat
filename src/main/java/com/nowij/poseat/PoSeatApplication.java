d81d063d29c7d97976592787b7db1eb916afc2cd
nowij <nowijdl@gmail.com>
2022-05-25 10:18:31 +0900

    project init

---
+++ src/main/java/com/nowij/poseat/PoSeatApplication.java
@@ -0,0 +1,13 @@
+package com.nowij.poseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoSeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoSeatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nowij/poseat/PoSeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowij.poseat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoSeatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
